ec8f346876c781481fc77754a6f71f1a7f88964d
Volker Brömmel <wbommel@users.noreply.github.com>
2019-09-12 11:38:23 +0200

    initial commit

---
+++ TrafficLogger/TrafficLogger.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace TrafficLogger
+{
+    partial class TrafficLogger
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrIntervall = new System.Windows.Forms.Timer(this.components);
+            this.trafficLog = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.trafficLog)).BeginInit();
+            // 
+            // tmrIntervall
+            // 
+            this.tmrIntervall.Interval = 60000;
+            this.tmrIntervall.Tick += new System.EventHandler(this.tmrIntervall_Tick);
+            // 
+            // TrafficLogger
+            // 
+            this.ServiceName = "TrafficLogger";
+            ((System.ComponentModel.ISupportInitialize)(this.trafficLog)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrIntervall;
+        private System.Diagnostics.EventLog trafficLog;
+    }
+}
---
+++ TrafficLogger/TrafficLogger.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficLogger
+{
+    public partial class TrafficLogger : ServiceBase
+    {
+        public TrafficLogger()
+        {
+            InitializeComponent();
+            trafficLog = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("TrafficLogger"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "TrafficLogger", "DayLog");
+            }
+            trafficLog.Source = "TrafficLogger";
+            trafficLog.Log = "DayLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            tmrIntervall.Start();
+            trafficLog.WriteEntry("TrafficLogger started", EventLogEntryType.Information);
+        }
+
+        protected override void OnContinue()
+        {
+            tmrIntervall.Start();
+            trafficLog.WriteEntry("TrafficLogger continued", EventLogEntryType.Information);
+            base.OnContinue();
+        }
+
+        protected override void OnStop()
+        {
+            tmrIntervall.Stop();
+            trafficLog.WriteEntry("TrafficLogger stopped", EventLogEntryType.Information);
+        }
+
+        protected override void OnPause()
+        {
+            tmrIntervall.Stop();
+            trafficLog.WriteEntry("TrafficLogger paused", EventLogEntryType.Information);
+            base.OnPause();
+        }
+
+        protected override void OnShutdown()
+        {
+            tmrIntervall.Stop();
+            trafficLog.WriteEntry("TrafficLogger stopped due to shutdown", EventLogEntryType.Information);
+            base.OnShutdown();
+        }
+
+        private void tmrIntervall_Tick(object sender, EventArgs e)
+        {
+            trafficLog.WriteEntry("Action", EventLogEntryType.Information, 1);
+        }
+    }
+}
