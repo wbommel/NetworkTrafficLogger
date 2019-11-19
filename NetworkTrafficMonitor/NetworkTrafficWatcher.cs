be20aaf8a4bd0a4e74166fce9978d962e4a963e1
Volker Brömmel <wbommel@users.noreply.github.com>
2019-11-19 20:37:37 +0100

    First working display of current state

--- NetworkTrafficMonitor/NetworkTrafficWatcher.cs
+++ NetworkTrafficMonitor/NetworkTrafficWatcher.cs
@@ -5,6 +5,7 @@ using System;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using vobsoft.net.model;
 
@@ -13,72 +14,31 @@ namespace NetworkTrafficMonitor
     public class NetworkTrafficWatcher : IDisposable
     {
         #region declarations
+        private string _fileName;
+        private TrafficData _trafficData = null;
+        private Machine _localMachine = null;
 
-        #endregion
-
-        #region constructor
-
-        #region dispose pattern accoring to MSDN article (https://docs.microsoft.com/de-de/dotnet/standard/garbage-collection/implementing-dispose)
-        // Flag: Has Dispose already been called?
-        bool disposed = false;
-        // Instantiate a SafeHandle instance.
-        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
-
-        // Public implementation of Dispose pattern callable by consumers.
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        // Protected implementation of Dispose pattern.
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposed)
-                return;
-
-            if (disposing)
-            {
-                handle.Dispose();
-                // Free any other managed objects here.
-                //
-                _trafficData = null;
-                _localMachine = null;
-                _fsw = null;
-            }
-
-            disposed = true;
-        }
-        #endregion
-        #endregion
-
-        #region private functions
-        #endregion
-
-        #region properties
-        #endregion
-
-        #region methods
-        #endregion
-    }
-
-
-
-    public class NetworkTrafficWatcherOld : IDisposable
-    {
-        #region declarations
-        private string _logFile;
-        private TrafficData _trafficData;
-        private Machine _localMachine;
-        private FileSystemWatcher _fsw;
-
-        private int _exceptionCount = 0;
         private int _ioExceptionCount = 0;
+        private int _exceptionCount = 0;
 
-        private bool _breakLoop = false;
+        #region events
+        #endregion
+        protected virtual void OnFileError(FileErrorEventArgs e)
+        {
+            EventHandler<FileErrorEventArgs> handler = FileError;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public event EventHandler<FileErrorEventArgs> FileError;
         #endregion
 
         #region constructor
+        public NetworkTrafficWatcher()
+        {
+        }
 
         #region dispose pattern accoring to MSDN article (https://docs.microsoft.com/de-de/dotnet/standard/garbage-collection/implementing-dispose)
         // Flag: Has Dispose already been called?
@@ -106,7 +66,6 @@ namespace NetworkTrafficMonitor
                 //
                 _trafficData = null;
                 _localMachine = null;
-                _fsw = null;
             }
 
             disposed = true;
@@ -118,13 +77,22 @@ namespace NetworkTrafficMonitor
         private void _reloadLogfile()
         {
             bool _fileWasRead = false;
+            int _timeout = 0;
+
             while (!_fileWasRead)
             {
+                if (_timeout == 30)
+                {
+                    OnFileError(new FileErrorEventArgs() { Type = FileErrorEventArgs.EventType.Timeout, IOExceptionCount = _ioExceptionCount, ExceptionCount = _exceptionCount, LastMessage = "Timeout reached (30s)" });
+                    break;
+                }
+
                 try
                 {
                     _trafficData = null;
                     Thread.Sleep(1000);
-                    _trafficData = JsonConvert.DeserializeObject<TrafficData>(File.ReadAllText(_logFile));
+                    _trafficData = JsonConvert.DeserializeObject<TrafficData>(File.ReadAllText(_fileName));
+                    if (_trafficData.Machines.ContainsKey(Environment.MachineName)) _localMachine = _trafficData.Machines[Environment.MachineName];
                     _fileWasRead = true;
                 }
                 catch (IOException e)
@@ -132,7 +100,7 @@ namespace NetworkTrafficMonitor
                     if (!string.IsNullOrEmpty(e.Message))
                     {
                         _ioExceptionCount++;
-                        Console.WriteLine("IOExceptions: " + _ioExceptionCount + "    Exceptions: " + _exceptionCount);
+                        OnFileError(new FileErrorEventArgs() { Type = FileErrorEventArgs.EventType.Timeout, IOExceptionCount = _ioExceptionCount, ExceptionCount = _exceptionCount, LastMessage = e.Message });
                     }
                 }
                 catch (Exception ex)
@@ -140,123 +108,68 @@ namespace NetworkTrafficMonitor
                     if (!string.IsNullOrEmpty(ex.Message))
                     {
                         _exceptionCount++;
-                        Console.WriteLine("IOExceptions: " + _ioExceptionCount + "    Exceptions: " + _exceptionCount);
+                        OnFileError(new FileErrorEventArgs() { Type = FileErrorEventArgs.EventType.Timeout, IOExceptionCount = _ioExceptionCount, ExceptionCount = _exceptionCount, LastMessage = ex.Message });
                     }
                 }
+
+                _timeout++;
             }
         }
 
-        private void Fsw_Changed(object sender, FileSystemEventArgs e)
+        private Reading _getNewestReading(LocalNetworkInterface lni)
         {
-            if (e.Name == Path.GetFileName(_logFile))
+            long lastReadingTime = 0;
+            Reading lastReading = null;
+            foreach (var reading in lni.Readings.Values)
             {
-                _reloadLogfile();
-                _showNetworkUsage();
-            }
-        }
-
-        private void _showNetworkUsage()
-        {
-            string nl = Environment.NewLine;
-
-            Console.SetCursorPosition(0, 2);
-
-            string strFormat = "{0,-30}{1,20}{2,20}{3,20}";
-            Console.WriteLine(string.Format(strFormat, "Interface Name", "Bytes Received", "Bytes Sent", "Bytes Overall"));
-            Console.WriteLine("------------------------------------------------------------------------------------------");
-
-
-            foreach (var localNI in _localMachine.Interfaces.Values)
-            {
-                if (Settings.Default.ShowActiveAdaptersOnly && localNI.Status.ToUpper() != "UP") { continue; }
-
-                //var lastReading = localNI.Readings.Values.OrderBy(x => x.LogTime).Last();
-                var lastReading = new Reading() { LogTime = 0 };
-                foreach (var r in localNI.Readings.Values)
-                {
-                    if (r.LogTime > lastReading.LogTime)
-                    {
-                        lastReading = r;
-                    }
-                }
-                Console.WriteLine(string.Format(strFormat, localNI.Name, lastReading.BytesReceived, lastReading.BytesSent, lastReading.BytesReceived + lastReading.BytesSent));
+                if (reading.LogTime > lastReadingTime) { lastReading = reading; }
             }
 
-
-            Console.WriteLine(nl);
-            Console.WriteLine(nl);
-            Console.WriteLine(nl);
-        }
-
-        private void _watchIt()
-        {
-            //get logfile
-            _logFile = Settings.Default.Logfile;
-
-#if DEBUG
-            _logFile = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\..\..\..\NetworkAdapterTest\bin\Debug\NetworkTraffic.json";
-#endif
-            if (!File.Exists(_logFile))
-            {
-                throw new FileNotFoundException("Logfile not found: " + _logFile);
-            }
-
-
-
-            _reloadLogfile();
-            if (_trafficData.Machines.ContainsKey(Environment.MachineName) && _trafficData.Machines[Environment.MachineName] is Machine)
-            {
-                //get machine
-                _localMachine = _trafficData.Machines[Environment.MachineName];
-
-                //initial Message
-                Console.SetCursorPosition(0, 0);
-                Console.WriteLine("Showing network statistics... (press x to exit)");
-                _showNetworkUsage();
-
-                //use FileSystemWatcher
-                using (_fsw = new FileSystemWatcher(Path.GetDirectoryName(_logFile)))
-                {
-                    _fsw.Changed += Fsw_Changed;
-                    _fsw.EnableRaisingEvents = true;
-
-                    //go to wait loop
-                    while (true)
-                    {
-                        if (Console.KeyAvailable)
-                        {
-                            ConsoleKeyInfo cki = Console.ReadKey();
-                            if (cki.Key == ConsoleKey.X) { break; }
-                        }
-
-                        if (_breakLoop) { break; }
-
-                        _reloadLogfile();
-                        _showNetworkUsage();
-
-                        Thread.Sleep(1000);
-                    }
-
-                    _fsw.EnableRaisingEvents = false;
-                    _fsw.Changed -= Fsw_Changed;
-                }
-            }
+            return lastReading;
         }
         #endregion
 
         #region properties
+        public string TestOutput
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var ni in _localMachine.Interfaces.Values)
+                {
+                    Reading r = _getNewestReading(ni);
+                    sb.Append(ni.Name + " - Readings: " + ni.Readings.Count + "   Bytes Received: " + r.BytesReceived + Environment.NewLine);
+                }
+                return sb.ToString();
+            }
+        }
         #endregion
 
         #region methods
-        public void StartWatching()
+        public void ReadTrafficData(string fileName)
         {
-            _watchIt();
-        }
-
-        public void StopWatching()
-        {
-            _breakLoop = true;
+            _fileName = fileName;
+            _reloadLogfile();
         }
         #endregion
     }
+
+
+
+    #region event args
+    public class FileErrorEventArgs
+    {
+        public enum EventType
+        {
+            Timeout,
+            IOException,
+            Exception
+        }
+
+        public EventType Type { get; set; }
+        public int IOExceptionCount { get; set; }
+        public int ExceptionCount { get; set; }
+        public string LastMessage { get; set; }
+    }
+    #endregion
 }
--- NetworkTrafficMonitor/Program.cs
+++ NetworkTrafficMonitor/Program.cs
@@ -12,79 +12,62 @@ namespace NetworkTrafficMonitor
     class Program
     {
         private static string _logFile;
-        private static TrafficData _trafficData;
-        private static Machine _localMachine;
+        //private static TrafficData _trafficData;
+        //private static Machine _localMachine;
         private static FileSystemWatcher _fsw;
 
-        private static int _exceptionCount = 0;
-        private static int _ioExceptionCount = 0;
+        //private static int _exceptionCount = 0;
+        //private static int _ioExceptionCount = 0;
 
         static void Main(string[] args)
         {
-            var t = new NetworkTrafficWatcher();
-            t.StartWatching();
+            //get logfile
+            _logFile = Settings.Default.Logfile;
 
-//            //get logfile
-//            _logFile = Settings.Default.Logfile;
+#if DEBUG
+            _logFile = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\..\..\..\NetworkAdapterTest\bin\Debug\NetworkTraffic.json";
+#endif
+            if (!File.Exists(_logFile))
+            {
+                Console.WriteLine("Logfile not found. Exitting.");
+                _anyKey();
+                Console.ReadKey();
+                return;
+            }
 
-//#if DEBUG
-//            _logFile = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\..\..\..\NetworkAdapterTest\bin\Debug\NetworkTraffic.json";
-//#endif
-//            if (!File.Exists(_logFile))
-//            {
-//                Console.WriteLine("Logfile not found. Exitting.");
-//                _anyKey();
-//                Console.ReadKey();
-//                return;
-//            }
+            //initial Message
+            _initialMessage();
 
+            //init FileSystemWatcher
+            _fsw = new FileSystemWatcher(Path.GetDirectoryName(_logFile));
+            _fsw.Changed += Fsw_Changed;
+            _fsw.EnableRaisingEvents = true;
 
+            //different approach than FSW: remember time
+            //DateTime dtLast = DateTime.Now;
 
-//            _reloadLogfile();
-//            if (_trafficData.Machines.ContainsKey(Environment.MachineName) && _trafficData.Machines[Environment.MachineName] is Machine)
-//            {
-//                //get machine
-//                _localMachine = _trafficData.Machines[Environment.MachineName];
+            //go to wait loop
+            while (true)
+            {
+                //_writeInterfaceStats();
 
-//                //initial Message
-//                Console.SetCursorPosition(0, 0);
-//                Console.WriteLine("Showing network statistics... (press x to exit)");
-//                _showNetworkUsage();
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo cki = Console.ReadKey();
+                    if (cki.Key == ConsoleKey.X) { break; }
+                }
 
-//                //init FileSystemWatcher
-//                _fsw = new FileSystemWatcher(Path.GetDirectoryName(_logFile));
-//                _fsw.Changed += Fsw_Changed;
-//                _fsw.EnableRaisingEvents = true;
+                //check if statistics need refresh
+                //if (DateTime.Now - dtLast >= TimeSpan.FromSeconds(5))
+                //{
+                //    _reloadLogfile();
+                //    _showNetworkUsage();
+                //    dtLast = DateTime.Now;
+                //}
 
-//                //different approach than FSW: remember time
-//                //DateTime dtLast = DateTime.Now;
+                Thread.Sleep(1000);
+            }
 
-//                //go to wait loop
-//                while (true)
-//                {
-//                    //_writeInterfaceStats();
-
-//                    if (Console.KeyAvailable)
-//                    {
-//                        ConsoleKeyInfo cki = Console.ReadKey();
-//                        if (cki.Key == ConsoleKey.X) { break; }
-//                    }
-
-//                    //check if statistics need refresh
-//                    //if (DateTime.Now - dtLast >= TimeSpan.FromSeconds(5))
-//                    //{
-//                    //    _reloadLogfile();
-//                    //    _showNetworkUsage();
-//                    //    dtLast = DateTime.Now;
-//                    //}
-
-//                    Thread.Sleep(1000);
-//                }
-
-//                _fsw.EnableRaisingEvents = false;
-//                _fsw.Changed -= Fsw_Changed;
-//                _fsw.Dispose();
-//            }
 
 
 
@@ -117,14 +100,27 @@ namespace NetworkTrafficMonitor
         //    }
         //}
 
-        //private static void Fsw_Changed(object sender, FileSystemEventArgs e)
-        //{
-        //    if (e.Name == Path.GetFileName(_logFile))
-        //    {
-        //        _reloadLogfile();
-        //        _showNetworkUsage();
-        //    }
-        //}
+
+        private static void _initialMessage()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("Showing network statistics... (press x to exit)");
+        }
+
+        private static void Fsw_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (e.Name == Path.GetFileName(_logFile))
+            {
+                using (var ntw = new NetworkTrafficWatcher())
+                {
+                    ntw.ReadTrafficData(_logFile);
+                    Console.Clear();
+                    _initialMessage();
+                    Console.SetCursorPosition(0, 2);
+                    Console.WriteLine(ntw.TestOutput);
+                }
+            }
+        }
 
         //private static void _showNetworkUsage()
         //{
