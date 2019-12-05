ba6ba8a86754085962ce49ab03f87f5d1d628117
Volker Brömmel <wbommel@users.noreply.github.com>
2019-12-05 12:18:56 +0100

    JSON to LiteDB

---
+++ JSON2LiteDB/Program.cs
@@ -0,0 +1,47 @@
+﻿using LiteDB;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vobsoft.net.model;
+
+namespace JSON2LiteDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonFilename = @"E:\temp2\NetworkTraficTest\NetworkTraffic.json";
+            string liteDBFilename = @"E:\temp2\NetworkTraficTest\NetworkTraffic.db";
+
+            JSONData2LiteDB(_loadJSONFile(jsonFilename), liteDBFilename);
+
+            Console.WriteLine("any key to exit");
+            Console.ReadKey();
+        }
+
+
+
+        private static TrafficData _loadJSONFile(string filename)
+        {
+            return JsonConvert.DeserializeObject<TrafficData>(File.ReadAllText(filename));
+        }
+
+        private static void JSONData2LiteDB(TrafficData trafficJSON, string liteDBFilename)
+        {
+            using (var db = new LiteDatabase(liteDBFilename))
+            {
+                foreach (var machineData in trafficJSON.Machines.Values)
+                {
+                    var dbmachines = db.GetCollection("machines");
+                    dbmachines.Insert<vobsoft.net.LiteDBLogger.model.Machine>(new vobsoft.net.LiteDBLogger.model.Machine() {
+                        MachineName=Environment.MachineName
+                    });
+                }
+            }
+        }
+    }
+}
