f9cbfdd4734225893ca0eb76b97bffb2433c9af2
Volker Brömmel <wbommel@users.noreply.github.com>
2019-12-06 11:14:25 +0100

    Data transformation

--- JSON2LiteDB/Program.cs
+++ JSON2LiteDB/Program.cs
@@ -36,10 +36,58 @@ namespace JSON2LiteDB
             {
                 foreach (var machineData in trafficJSON.Machines.Values)
                 {
-                    var dbmachines = db.GetCollection("machines");
-                    dbmachines.Insert<vobsoft.net.LiteDBLogger.model.Machine>(new vobsoft.net.LiteDBLogger.model.Machine() {
-                        MachineName=Environment.MachineName
-                    });
+                    Console.WriteLine(Environment.NewLine);
+                    string format = "Machine created - ID: {0,-5} Name: {1,-30}";
+
+                    //first create Machine
+                    var dbmachines = db.GetCollection<vobsoft.net.LiteDBLogger.model.Machine>("machines");
+
+                    var newMachine = new vobsoft.net.LiteDBLogger.model.Machine()
+                    {
+                        MachineName = Environment.MachineName
+                    };
+                    dbmachines.Insert(newMachine);
+
+                    Console.WriteLine(string.Format(format, newMachine.Id, newMachine.MachineName));
+
+                    foreach (var lni in machineData.Interfaces.Values)
+                    {
+                        Console.WriteLine(Environment.NewLine);
+                        format = "Interface created - ID: {0,-5} Name: {1,-24} Description: {2,-32}";
+
+                        var dbInterfaces = db.GetCollection<vobsoft.net.LiteDBLogger.model.LocalNetworkInterface>("interfaces");
+
+                        var newInterface = new vobsoft.net.LiteDBLogger.model.LocalNetworkInterface()
+                        {
+                            MachineId = newMachine.Id,
+                            Name = lni.Name,
+                            InterfaceGUID = lni.InterfaceId,
+                            Description = lni.Description,
+                            Speed = lni.Speed,
+                            Status = lni.Status,
+                            Type = lni.Type
+                        };
+                        dbInterfaces.Insert(newInterface);
+
+                        Console.WriteLine(format, newInterface.Id, newInterface.Name, newInterface.Description);
+
+                        foreach (var reading in lni.Readings.Values)
+                        {
+                            format = "Reading created - ID: {0,-5} Logtime: {1,-14} Sent+Recieved: {2,-14}";
+
+                            var dbReadings = db.GetCollection<vobsoft.net.LiteDBLogger.model.Reading>("readings");
+
+                            var newReading = new vobsoft.net.LiteDBLogger.model.Reading()
+                            {
+                                InterfaceId = newInterface.Id,
+                                LogTime = reading.LogTime,
+                                BytesReceived = reading.BytesReceived,
+                                BytesSent = reading.BytesSent
+                            };
+
+                            Console.WriteLine(format, newReading.Id, newReading.LogTime, newReading.BytesSent + newReading.BytesReceived);
+                        }
+                    }
                 }
             }
         }
